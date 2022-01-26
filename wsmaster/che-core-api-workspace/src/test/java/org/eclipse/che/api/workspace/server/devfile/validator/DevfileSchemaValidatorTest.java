422bbd652d99efefc5965f57f43bfe0f28dbb445
Ilya Buziuk <ibuziuk@redhat.com>
2022-01-26 14:16:57 +0100

    fix: updating devfile 2.2 schema to the latest version
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/validator/DevfileSchemaValidatorTest.java
+++ wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/validator/DevfileSchemaValidatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -88,6 +88,10 @@ public class DevfileSchemaValidatorTest {
       {"devfile/devfile_v2-1-0_simple-devfile.yaml"},
       {"devfile/devfile_v2-2-0-simple-devfile.yaml"},
       {"devfile/devfile_v2-2-0-quarkus.yaml"},
+      {"devfile/devfile_v2-2-0-basic-nodejs.yaml"},
+      {"devfile/devfile_v2-2-0-basic-python.yaml"},
+      {"devfile/devfile_v2-2-0-basic-quarkus.yaml"},
+      {"devfile/devfile_v2-2-0-basic-springboot.yaml"}
     };
   }
 

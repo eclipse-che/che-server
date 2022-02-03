b400bb62c5667d5535fc1a54ba14b30bc138519c
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2022-02-03 12:27:17 +0200

    Fix: behavior for unexisting files for public repositories (#243) (#253)
    * Fix behavior for unexisting files for public repositories (#243)
    
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- wsmaster/che-core-api-auth-github/src/main/java/org/eclipse/che/security/oauth/GitHubOAuthAuthenticatorProvider.java
+++ wsmaster/che-core-api-auth-github/src/main/java/org/eclipse/che/security/oauth/GitHubOAuthAuthenticatorProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -70,8 +70,8 @@ public class GitHubOAuthAuthenticatorProvider implements Provider<OAuthAuthentic
         && !isNullOrEmpty(tokenUri)
         && Objects.nonNull(redirectUris)
         && redirectUris.length != 0) {
-      String clientId = Files.readString(Path.of(clientIdPath));
-      String clientSecret = Files.readString(Path.of(clientSecretPath));
+      final String clientId = Files.readString(Path.of(clientIdPath)).trim();
+      final String clientSecret = Files.readString(Path.of(clientSecretPath)).trim();
       if (!isNullOrEmpty(clientId) && !isNullOrEmpty(clientSecret)) {
         return new GitHubOAuthAuthenticator(
             clientId, clientSecret, redirectUris, authUri, tokenUri);

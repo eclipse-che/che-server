09b56c7e08cfddfb4b2415492bbf595effa888be
Ilya Buziuk <ibuziuk@redhat.com>
2022-04-04 12:57:17 +0200

    fix: NPE fixup in the SshKeysConfigurator + bumping devfile 2.2 schema to teh latest version
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/namespace/configurator/SshKeysConfigurator.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/namespace/configurator/SshKeysConfigurator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -131,7 +131,7 @@ public class SshKeysConfigurator implements NamespaceConfigurator {
 
     for (SshPair sshPair : sshPairs) {
       sshConfigData.append(buildConfig(sshPair.getName()));
-      if (!isNullOrEmpty(sshPair.getPrivateKey())) {
+      if (!isNullOrEmpty(sshPair.getPrivateKey()) && !isNullOrEmpty(sshPair.getPublicKey())) {
         data.put(
             sshPair.getName(),
             Base64.getEncoder().encodeToString(sshPair.getPrivateKey().getBytes()));

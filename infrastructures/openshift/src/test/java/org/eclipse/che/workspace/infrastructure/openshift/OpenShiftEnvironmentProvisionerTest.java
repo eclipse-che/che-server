d33ae2d6d687c386bf548f7823ac060f33ca31ff
Andrew Obuchowicz <aobuchow@redhat.com>
2022-08-02 12:25:54 -0400

    Remove che.infra.kubernetes.async.storage.image, che.infra.kubernetes.async.storage.shutdown_timeout_min, che.infra.kubernetes.async.storage.shutdown_check_period_min properties and some PVC-related classes
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/OpenShiftEnvironmentProvisionerTest.java
+++ infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/OpenShiftEnvironmentProvisionerTest.java
@@ -17,8 +17,6 @@ import static org.mockito.Mockito.when;
 
 import org.eclipse.che.api.core.model.workspace.runtime.RuntimeIdentity;
 import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.WorkspaceVolumesStrategy;
-import org.eclipse.che.workspace.infrastructure.kubernetes.provision.AsyncStoragePodInterceptor;
-import org.eclipse.che.workspace.infrastructure.kubernetes.provision.AsyncStorageProvisioner;
 import org.eclipse.che.workspace.infrastructure.kubernetes.provision.CertificateProvisioner;
 import org.eclipse.che.workspace.infrastructure.kubernetes.provision.DeploymentMetadataProvisioner;
 import org.eclipse.che.workspace.infrastructure.kubernetes.provision.GatewayRouterProvisioner;
@@ -72,8 +70,6 @@ public class OpenShiftEnvironmentProvisionerTest {
   @Mock private ImagePullSecretProvisioner imagePullSecretProvisioner;
   @Mock private ProxySettingsProvisioner proxySettingsProvisioner;
   @Mock private ServiceAccountProvisioner serviceAccountProvisioner;
-  @Mock private AsyncStorageProvisioner asyncStorageProvisioner;
-  @Mock private AsyncStoragePodInterceptor asyncStoragePodObserver;
   @Mock private CertificateProvisioner certificateProvisioner;
   @Mock private SshKeysProvisioner sshKeysProvisioner;
   @Mock private GitConfigProvisioner gitConfigProvisioner;
@@ -107,8 +103,6 @@ public class OpenShiftEnvironmentProvisionerTest {
             proxySettingsProvisioner,
             nodeSelectorProvisioner,
             tolerationsProvisioner,
-            asyncStorageProvisioner,
-            asyncStoragePodObserver,
             serviceAccountProvisioner,
             certificateProvisioner,
             sshKeysProvisioner,

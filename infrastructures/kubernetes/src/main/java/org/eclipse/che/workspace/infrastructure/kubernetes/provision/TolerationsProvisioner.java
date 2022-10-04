7d4121efa529990e21f6c766ff9e8375e32f79cc
Andrew Obuchowicz <aobuchow@redhat.com>
2022-10-04 18:57:21 -0400

    chore: remove che.workspace.pod.tolerations_json property
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/TolerationsProvisioner.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/TolerationsProvisioner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -13,17 +13,12 @@ package org.eclipse.che.workspace.infrastructure.kubernetes.provision;
 
 import static java.util.Collections.emptyList;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.Toleration;
 import java.util.List;
 import javax.inject.Inject;
-import javax.inject.Named;
 import org.eclipse.che.api.core.model.workspace.runtime.RuntimeIdentity;
 import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
-import org.eclipse.che.commons.annotation.Nullable;
 import org.eclipse.che.inject.ConfigurationException;
 import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironment;
 
@@ -33,19 +28,8 @@ public class TolerationsProvisioner implements ConfigurationProvisioner {
   private final List<Toleration> tolerations;
 
   @Inject
-  public TolerationsProvisioner(
-      @Nullable @Named("che.workspace.pod.tolerations_json") String tolerationsProperty)
-      throws ConfigurationException {
-    try {
-      ObjectMapper jsonMapper = new ObjectMapper();
-      this.tolerations =
-          tolerationsProperty != null
-              ? jsonMapper.readValue(tolerationsProperty, new TypeReference<List<Toleration>>() {})
-              : emptyList();
-    } catch (JsonProcessingException e) {
-      throw new ConfigurationException(
-          "che.workspace.pod.tolerations_json contains an invalid JSON string", e);
-    }
+  public TolerationsProvisioner() throws ConfigurationException {
+    this.tolerations = emptyList();
   }
 
   @Override
--- infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/TolerationsProvisionerTest.java
+++ infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/TolerationsProvisionerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -11,17 +11,12 @@
  */
 package org.eclipse.che.workspace.infrastructure.kubernetes.provision;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
-import io.fabric8.kubernetes.api.model.Toleration;
-import java.util.Collections;
 import org.eclipse.che.api.core.model.workspace.runtime.RuntimeIdentity;
-import org.eclipse.che.inject.ConfigurationException;
 import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironment;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
@@ -43,33 +38,12 @@ public class TolerationsProvisionerTest {
   }
 
   @Test
-  public void shouldAddTolerationsIntoAllPods() throws Exception {
-    // given
-    k8sEnv.addPod(createPod("pod"));
-    k8sEnv.addPod(createPod("pod2"));
-    Toleration expectedToleration =
-        new Toleration("NoExecute", "a.node.taint", "Equal", 0L, "aValue");
-    ObjectMapper objMapper = new ObjectMapper();
-    String json = objMapper.writeValueAsString(Collections.singletonList(expectedToleration));
-    provisioner = new TolerationsProvisioner(json);
-
-    // when
-    provisioner.provision(k8sEnv, runtimeId);
-
-    // then
-    for (Pod pod : k8sEnv.getPodsCopy().values()) {
-      assertEquals(pod.getSpec().getTolerations().size(), 1);
-      assertEquals(pod.getSpec().getTolerations().get(0), expectedToleration);
-    }
-  }
-
-  @Test
   public void shouldOmitEmptyTolerations() throws Exception {
     // given
     k8sEnv.addPod(createPod("pod"));
     k8sEnv.addPod(createPod("pod2"));
 
-    provisioner = new TolerationsProvisioner(null);
+    provisioner = new TolerationsProvisioner();
 
     // when
     provisioner.provision(k8sEnv, runtimeId);
@@ -78,12 +52,6 @@ public class TolerationsProvisionerTest {
     for (Pod pod : k8sEnv.getPodsCopy().values()) {
       assertTrue(pod.getSpec().getTolerations().isEmpty());
     }
-  }
-
-  @Test(expectedExceptions = ConfigurationException.class)
-  public void shouldFailOnInvalidTolerationsJson() throws Exception {
-    // given
-    provisioner = new TolerationsProvisioner("an invalid json string");
   }
 
   private Pod createPod(String podName) {

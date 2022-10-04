9c6a43c0f9998fff76345a0b401581fd29bda342
Andrew Obuchowicz <aobuchow@redhat.com>
2022-10-04 16:36:44 -0400

    chore: remove che.workspace.activity_check_scheduler_period_s, che.workspace.activity_check_scheduler_delay_s, che.workspace.activity_cleanup_scheduler_period_s, che.workspace.activity_cleanup_scheduler_initial_delay_s properties
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- wsmaster/che-core-api-workspace-activity/src/main/java/org/eclipse/che/api/workspace/activity/inject/WorkspaceActivityModule.java
+++ wsmaster/che-core-api-workspace-activity/src/main/java/org/eclipse/che/api/workspace/activity/inject/WorkspaceActivityModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2022 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -13,7 +13,6 @@ package org.eclipse.che.api.workspace.activity.inject;
 
 import com.google.inject.AbstractModule;
 import org.eclipse.che.api.workspace.activity.JpaWorkspaceActivityDao;
-import org.eclipse.che.api.workspace.activity.WorkspaceActivityChecker;
 import org.eclipse.che.api.workspace.activity.WorkspaceActivityDao;
 import org.eclipse.che.api.workspace.activity.WorkspaceActivityManager;
 import org.eclipse.che.api.workspace.activity.WorkspaceActivityService;
@@ -26,7 +25,6 @@ public class WorkspaceActivityModule extends AbstractModule {
     // appropriately
     bind(WorkspaceActivityService.class);
     bind(WorkspaceActivityManager.class);
-    bind(WorkspaceActivityChecker.class);
     bind(WorkspaceActivityDao.class).to(JpaWorkspaceActivityDao.class);
   }
 }

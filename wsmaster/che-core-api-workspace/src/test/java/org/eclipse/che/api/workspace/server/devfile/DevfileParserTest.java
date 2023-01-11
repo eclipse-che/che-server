920b17b9aa791d8085d94bd12b1b96fafe28cb08
Nick Boldt <nboldt@redhat.com>
2023-01-11 11:37:28 -0400

    use eq(DevfileImpl.class); update copyright date; thanks to @vinokurig for the fix!
    Change-Id: I436df0c7f9e9557d348fc7faab44379fe510d643
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/DevfileParserTest.java
+++ wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/DevfileParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2023 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -194,7 +194,7 @@ public class DevfileParserTest {
     JsonProcessingException jsonException = mock(JsonProcessingException.class);
     when(jsonException.getMessage()).thenReturn("non valid");
     when(jsonMapper.readTree(anyString())).thenReturn(devfileJsonNode);
-    doThrow(jsonException).when(jsonMapper).treeToValue(any(), any());
+    doThrow(jsonException).when(jsonMapper).treeToValue(any(), eq(DevfileImpl.class));
 
     // when
     devfileParser.parseJson(DEVFILE_YAML_CONTENT);

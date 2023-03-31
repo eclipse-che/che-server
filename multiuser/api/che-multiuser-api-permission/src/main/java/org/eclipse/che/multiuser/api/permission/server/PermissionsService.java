da5541f28356716ac8fce53585cba11b44ee6553
Ilya Buziuk <ibuziuk@redhat.com>
2023-03-31 13:49:49 +0200

    chore: Adding @Deprecated annotation to the unused services that will be removed in the future versions
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- multiuser/api/che-multiuser-api-permission/src/main/java/org/eclipse/che/multiuser/api/permission/server/PermissionsService.java
+++ multiuser/api/che-multiuser-api-permission/src/main/java/org/eclipse/che/multiuser/api/permission/server/PermissionsService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2023 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -55,6 +55,7 @@ import org.eclipse.che.multiuser.api.permission.shared.model.PermissionsDomain;
  *
  * @author Sergii Leschenko
  */
+@Deprecated
 @Tag(name = "permissions", description = "Permissions REST API")
 @Path("/permissions")
 public class PermissionsService extends Service {

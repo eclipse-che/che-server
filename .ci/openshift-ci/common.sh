1cbef4b97059375bc65b8721af86bfb3090e87f9
Aleksandr Shmaraiev <oshmarai@redhat.com>
2023-04-20 16:42:00 +0300

    Check to get content of variables

--- .ci/openshift-ci/common.sh
+++ .ci/openshift-ci/common.sh
@@ -21,6 +21,16 @@ cat ${SHARED_DIR}/console.url
 echo "Content of custom-links.txt"
 cat ${SHARED_DIR}/custom-links.txt
 
+git remote add origin https://github.com/eclipse-che/che-server.git
+git fetch
+
+echo ${PULL_NUMBER}
+echo ${PULL_BASE_REF}
+echo ${PULL_BASE_SHA}
+echo ${PULL_SOURCE_REF}
+
+PR_COMMIT=$(echo ${PULL_PULL_SHA} | cut -c1-7)
+BRANCH=$(git branch --contains ${PR_COMMIT})
 BRANCH_NAME="che#21753"
 PR_IMAGE_TAG=$(git show origin/pr-number-${BRANCH_NAME}:shared-files/pr-image-tag)
 

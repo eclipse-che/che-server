aeeceb19e40c87a6341db9b92ad4b42c309340e3
Igor Vinokur <ivinokur@redhat.com>
2023-08-25 12:15:44 +0300

    Update io.fabric8.kubernetes-client version to latest
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/RouteServerResolverTest.java
+++ infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/RouteServerResolverTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2021 Red Hat, Inc.
+ * Copyright (c) 2012-2023 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -279,7 +279,7 @@ public class RouteServerResolverTest {
             new ServicePortBuilder()
                 .withPort(port)
                 .withNewTargetPort()
-                .withIntVal(port)
+                .withValue(port)
                 .endTargetPort()
                 .build())
         .endSpec()

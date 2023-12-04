eclipse-che/che-server

commit 5baa3a7ccf9beea6aa8c5468655073f05f3e265c

Align the unreachable devfile exception message (#615)
Add a common exception message and use it for cases when:

The devfile location is unavailable.
The devfile content is is not valid: e.g the response content is an html content of an scm authorisation page.

wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/urlfactory/URLFactoryBuilder.java
@@ -13,6 +13,7 @@ package org.eclipse.che.api.factory.server.urlfactory;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.eclipse.che.api.factory.server.ApiExceptionMapper.toApiException;
+import static org.eclipse.che.api.factory.server.scm.exception.ExceptionMessages.getDevfileConnectionErrorMessage;
 import static org.eclipse.che.api.factory.shared.Constants.CURRENT_VERSION;
 import static org.eclipse.che.api.workspace.server.devfile.Constants.CURRENT_API_VERSION;
 import static org.eclipse.che.api.workspace.shared.Constants.WORKSPACE_TOOLING_EDITOR_ATTRIBUTE;
@@ -130,7 +131,7 @@ public class URLFactoryBuilder {
         continue;
       } catch (DevfileException e) {
         LOG.debug("Unexpected devfile exception: {}", e.getMessage());
-        throw toApiException(e, location);
+        throw new ApiException(e.getMessage());
       }
       if (isNullOrEmpty(devfileYamlContent)) {
         return Optional.empty();
@@ -142,7 +143,7 @@ public class URLFactoryBuilder {
         try {
           devfileVersionDetector.devfileVersion(parsedDevfile);
         } catch (DevfileException e) {
-          throw new ApiException("Failed to fetch devfile");
+          throw new ApiException(getDevfileConnectionErrorMessage(devfileLocation));
         }
         return Optional.of(
             createFactory(parsedDevfile, overrideProperties, fileContentProvider, location));

wsmaster/che-core-api-factory/src/test/java/org/eclipse/che/api/factory/server/urlfactory/URLFactoryBuilderTest.java
@@ -441,7 +441,8 @@ public class URLFactoryBuilderTest {
                   }
                 }));
 
-    when(fileContentProvider.fetchContent(anyString())).thenThrow(new DevfileException("", cause));
+    when(fileContentProvider.fetchContent(anyString()))
+        .thenThrow(new DevfileException(expectedMessage, cause));
 
     // when
     try {
@@ -457,7 +458,7 @@ public class URLFactoryBuilderTest {
 
   @Test(
       expectedExceptions = ApiException.class,
-      expectedExceptionsMessageRegExp = "Failed to fetch devfile")
+      expectedExceptionsMessageRegExp = "Could not reach devfile at location")
   public void shouldThrowErrorOnUnsupportedDevfileContent()
       throws ApiException, DevfileException, IOException {
     JsonNode jsonNode = mock(JsonNode.class);

.ci/openshift-ci/common.sh
@@ -102,6 +102,15 @@ testFactoryResolverNoPatOAuth() {
   testFactoryResolverResponse $1 200
 
   echo "[INFO] Check factory resolver for private repository with NO PAT/OAuth setup"
+  testFactoryResolverResponse $2 500
+}
+
+# check that raw devfile url factory resolver returns correct value without any PAT/OAuth setup
+testFactoryResolverNoPatOAuthRaw() {
+  echo "[INFO] Check factory resolver for public repository with NO PAT/OAuth setup"
+  testFactoryResolverResponse $1 200
+
+  echo "[INFO] Check factory resolver for private repository with NO PAT/OAuth setup"
   testFactoryResolverResponse $2 400
 }
 

.ci/openshift-ci/test-azure-no-pat-oauth-flow.sh
@@ -26,8 +26,7 @@ source "${SCRIPT_DIR}"/common.sh
 trap "catchFinish" EXIT SIGINT
 
 setupTestEnvironment ${OCP_NON_ADMIN_USER_NAME}
-# due to the issue https://github.com/eclipse/che/issues/22469
-# testFactoryResolverNoPatOAuth ${PUBLIC_REPO_URL} ${PRIVATE_REPO_URL}
+testFactoryResolverNoPatOAuth ${PUBLIC_REPO_URL} ${PRIVATE_REPO_URL}
 testCloneGitRepoProjectShouldExists ${PUBLIC_REPO_WORKSPACE_NAME} ${PUBLIC_PROJECT_NAME} ${PUBLIC_REPO_URL} ${USER_CHE_NAMESPACE}
 deleteTestWorkspace ${PUBLIC_REPO_WORKSPACE_NAME} ${USER_CHE_NAMESPACE}
 testCloneGitRepoNoProjectExists ${PRIVATE_REPO_WORKSPACE_NAME} ${PRIVATE_PROJECT_NAME} ${PRIVATE_REPO_URL} ${USER_CHE_NAMESPACE}

.ci/openshift-ci/test-bitbucket-no-pat-oauth-flow-raw-devfile-url.sh
@@ -26,7 +26,7 @@ source "${SCRIPT_DIR}"/common.sh
 trap "catchFinish" EXIT SIGINT
 
 setupTestEnvironment ${OCP_NON_ADMIN_USER_NAME}
-testFactoryResolverNoPatOAuth ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
+testFactoryResolverNoPatOAuthRaw ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
 
 testCloneGitRepoProjectShouldExists ${PUBLIC_REPO_WORKSPACE_NAME} ${PUBLIC_PROJECT_NAME} ${PUBLIC_REPO_RAW_PATH_URL} ${USER_CHE_NAMESPACE}
 deleteTestWorkspace ${PUBLIC_REPO_WORKSPACE_NAME} ${USER_CHE_NAMESPACE}

.ci/openshift-ci/test-github-no-pat-oauth-flow-raw-devfile-url.sh
@@ -26,7 +26,7 @@ source "${SCRIPT_DIR}"/common.sh
 trap "catchFinish" EXIT SIGINT
 
 setupTestEnvironment ${OCP_NON_ADMIN_USER_NAME}
-testFactoryResolverNoPatOAuth ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
+testFactoryResolverNoPatOAuthRaw ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
 
 testCloneGitRepoNoProjectExists ${PUBLIC_REPO_WORKSPACE_NAME} ${PUBLIC_PROJECT_NAME} ${PUBLIC_REPO_RAW_PATH_URL} ${USER_CHE_NAMESPACE}
 deleteTestWorkspace ${PUBLIC_REPO_WORKSPACE_NAME} ${USER_CHE_NAMESPACE}

.ci/openshift-ci/test-gitlab-no-pat-oauth-flow-raw-devfile-url.sh
@@ -26,7 +26,7 @@ source "${SCRIPT_DIR}"/common.sh
 trap "catchFinish" EXIT SIGINT
 
 setupTestEnvironment ${OCP_NON_ADMIN_USER_NAME}
-testFactoryResolverNoPatOAuth ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
+testFactoryResolverNoPatOAuthRaw ${PUBLIC_REPO_RAW_PATH_URL} ${PRIVATE_REPO_RAW_PATH_URL}
 
 testCloneGitRepoNoProjectExists ${PUBLIC_REPO_WORKSPACE_NAME} ${PUBLIC_PROJECT_NAME} ${PUBLIC_REPO_RAW_PATH_URL} ${USER_CHE_NAMESPACE}
 deleteTestWorkspace ${PUBLIC_REPO_WORKSPACE_NAME} ${USER_CHE_NAMESPACE}

wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/scm/AuthorizingFileContentProvider.java
@@ -13,6 +13,7 @@ package org.eclipse.che.api.factory.server.scm;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.eclipse.che.api.factory.server.scm.PersonalAccessTokenFetcher.OAUTH_2_PREFIX;
+import static org.eclipse.che.api.factory.server.scm.exception.ExceptionMessages.getDevfileConnectionErrorMessage;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -123,7 +124,7 @@ public class AuthorizingFileContentProvider<T extends RemoteFactoryUrl>
         }
       }
       throw new DevfileException(
-          "Could not reach devfile at " + "`" + exception.getMessage() + "`", exception);
+          getDevfileConnectionErrorMessage(exception.getMessage()), exception);
     }
   }
 

commit ebca10356e75b5ad1f6dcf2ec97b7937dbb12056

Throw the API exception with authorization URL on SCMunauthorised error (#621)
Fixes a bug with broken oauth flow caused by https://github.com/eclipse-che/che-server/pull/615

wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/urlfactory/URLFactoryBuilder.java
@@ -30,6 +30,7 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 import org.eclipse.che.api.core.ApiException;
 import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.factory.server.scm.exception.ScmUnauthorizedException;
 import org.eclipse.che.api.factory.server.urlfactory.RemoteFactoryUrl.DevfileLocation;
 import org.eclipse.che.api.factory.shared.dto.FactoryDevfileV2Dto;
 import org.eclipse.che.api.factory.shared.dto.FactoryDto;
@@ -131,7 +132,9 @@ public class URLFactoryBuilder {
         continue;
       } catch (DevfileException e) {
         LOG.debug("Unexpected devfile exception: {}", e.getMessage());
-        throw new ApiException(e.getMessage());
+        throw e.getCause() instanceof ScmUnauthorizedException
+            ? toApiException(e, location)
+            : new ApiException(e.getMessage());
       }
       if (isNullOrEmpty(devfileYamlContent)) {
         return Optional.empty();

wsmaster/che-core-api-factory/src/test/java/org/eclipse/che/api/factory/server/urlfactory/URLFactoryBuilderTest.java
@@ -451,7 +451,7 @@ public class URLFactoryBuilderTest {
     } catch (ApiException e) {
       assertTrue(e.getClass().isAssignableFrom(expectedClass));
       assertEquals(e.getMessage(), expectedMessage);
-      if (e.getServiceError() instanceof ExtendedError)
+      if ("SCM Authentication required".equals(e.getMessage()))
         assertEquals(((ExtendedError) e.getServiceError()).getAttributes(), expectedAttributes);
     }
   }
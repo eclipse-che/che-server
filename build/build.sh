3ecfefd12a4cf3b72b6b3755ad357ebe248d20bd
Anatolii Bazko <abazko@redhat.com>
2024-10-17 09:23:49 +0200

    Fixup
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- build/build.sh
+++ build/build.sh
@@ -180,7 +180,7 @@ build_image() {
     -e "s;\${BUILD_PREFIX};${PREFIX};" \
     -e "s;\${BUILD_TAG};${TAG};" \
     > ${DIR}/.Dockerfile
-  cd "${DIR}" && "${BUILDER}" "${BUILD_COMMAND}" ${PLATFORMS_ARG} -f ${DIR}/.Dockerfile -t ${IMAGE_NAME} ${BUILD_ARGS} .
+  cd "${DIR}" && "${BUILDER}" "${BUILD_COMMAND}" ${BUILD_PLATFORMS_ARG} -f ${DIR}/.Dockerfile -t ${IMAGE_NAME} ${BUILD_ARGS} .
   DOCKER_BUILD_STATUS=$?
   rm ${DIR}/.Dockerfile
   if [ $DOCKER_BUILD_STATUS -eq 0 ]; then

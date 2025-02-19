0372ac8c5ba61c132ed57f764c1718ba33e4a127
ivinokur <ivinokur@redhat.com>
2025-02-19 14:48:03 +0200

    Add Azure DevOps Server support

--- wsmaster/che-core-api-factory-azure-devops/src/test/java/org/eclipse/che/api/factory/server/azure/devops/AzureDevOpsURLParserTest.java
+++ wsmaster/che-core-api-factory-azure-devops/src/test/java/org/eclipse/che/api/factory/server/azure/devops/AzureDevOpsURLParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2023 Red Hat, Inc.
+ * Copyright (c) 2012-2025 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.mock;
 import static org.testng.Assert.assertEquals;
 
 import java.util.Optional;
+import org.eclipse.che.api.factory.server.scm.PersonalAccessTokenManager;
 import org.eclipse.che.api.factory.server.urlfactory.DevfileFilenamesProvider;
 import org.mockito.testng.MockitoTestNGListener;
 import org.testng.annotations.BeforeMethod;
@@ -31,7 +32,10 @@ public class AzureDevOpsURLParserTest {
   @BeforeMethod
   protected void start() {
     azureDevOpsURLParser =
-        new AzureDevOpsURLParser(mock(DevfileFilenamesProvider.class), "https://dev.azure.com/");
+        new AzureDevOpsURLParser(
+            mock(DevfileFilenamesProvider.class),
+            mock(PersonalAccessTokenManager.class),
+            "https://dev.azure.com/");
   }
 
   @Test(expectedExceptions = IllegalArgumentException.class)
@@ -41,6 +45,23 @@ public class AzureDevOpsURLParserTest {
 
   @Test(dataProvider = "parsing")
   public void testParse(
+      String url,
+      String organization,
+      String project,
+      String repository,
+      String branch,
+      String tag) {
+    AzureDevOpsUrl azureDevOpsUrl = azureDevOpsURLParser.parse(url);
+
+    assertEquals(azureDevOpsUrl.getOrganization(), organization);
+    assertEquals(azureDevOpsUrl.getProject(), project);
+    assertEquals(azureDevOpsUrl.getRepository(), repository);
+    assertEquals(azureDevOpsUrl.getBranch(), branch);
+    assertEquals(azureDevOpsUrl.getTag(), tag);
+  }
+
+  @Test(dataProvider = "parsingServer")
+  public void testParseServer(
       String url,
       String organization,
       String project,
@@ -190,6 +211,144 @@ public class AzureDevOpsURLParserTest {
         "MyTag"
       },
       {"https://MyOrg@dev.azure.com/MyOrg/_git/MyRepo", "MyOrg", "MyRepo", "MyRepo", null, null},
+    };
+  }
+
+  @DataProvider(name = "parsingServer")
+  public Object[][] expectedServerParsing() {
+    return new Object[][] {
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo.git",
+        "MyOrg",
+        "MyProject",
+        "MyRepo.git",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo.dot.git",
+        "MyOrg",
+        "MyProject",
+        "MyRepo.dot.git",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo-with-hypen",
+        "MyOrg",
+        "MyProject",
+        "MyRepo-with-hypen",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/-", "MyOrg", "MyProject", "-", null, null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/-j.git",
+        "MyOrg",
+        "MyProject",
+        "-j.git",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo?path=MyFile&version=GBmain&_a=contents",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        "main",
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/MyRepo",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/MyRepo.git",
+        "MyOrg",
+        "MyProject",
+        "MyRepo.git",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/MyRepo.dot.git",
+        "MyOrg",
+        "MyProject",
+        "MyRepo.dot.git",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/MyRepo",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/MyRepo-with-hypen",
+        "MyOrg",
+        "MyProject",
+        "MyRepo-with-hypen",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/-",
+        "MyOrg",
+        "MyProject",
+        "-",
+        null,
+        null
+      },
+      {
+        "ssh://dev.azure-server.com:22/MyOrg/MyProject/_git/-j.git",
+        "MyOrg",
+        "MyProject",
+        "-j.git",
+        null,
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo?path=MyFile&version=GBmain",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        "main",
+        null
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo?path=MyFile&version=GTMyTag&_a=contents",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        null,
+        "MyTag"
+      },
+      {
+        "https://dev.azure-server.com/MyOrg/MyProject/_git/MyRepo?path=MyFile&version=GTMyTag",
+        "MyOrg",
+        "MyProject",
+        "MyRepo",
+        null,
+        "MyTag"
+      },
+      {"https://dev.azure-server.com/MyOrg/_git/MyRepo", "MyOrg", "MyRepo", "MyRepo", null, null},
     };
   }
 

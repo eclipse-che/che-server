71d6b0a389971b6118fd04a880d8cfcd70e0eeb1
Oleksii Orel <oorel@redhat.com>
2026-07-28 17:03:02 +0300

    feat(oauth): expose clientId in OAuthAuthenticatorDescriptor (#1035)
    Add clientId field to OAuthAuthenticatorDescriptor DTO so clients
    can initiate OAuth flows that require the client ID directly (e.g.
    GitHub Device Authorization Flow / RFC 8628) without needing access
    to the raw Kubernetes Secret.
    
    - OAuthAuthenticator: store clientId passed to configure() and
      expose it via getClientId()
    - EmbeddedOAuthAPI: populate clientId in the descriptor returned by
      GET /api/oauth
    - OAuthAuthenticatorDescriptor: new clientId getter/setter/builder
    
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- wsmaster/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/EmbeddedOAuthAPI.java
+++ wsmaster/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/EmbeddedOAuthAPI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2025 Red Hat, Inc.
+ * Copyright (c) 2012-2026 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -206,13 +206,16 @@ public class EmbeddedOAuthAPI implements OAuthAPI {
                   .withRequired(true)
                   .withDefaultValue("federated_login")));
       OAuthAuthenticator authenticator = oauth2Providers.getAuthenticator(name);
+      String endpointUrl =
+          authenticator != null
+              ? authenticator.getEndpointUrl()
+              : oauth1Providers.getAuthenticator(name).getEndpointUrl();
+      String clientId = authenticator != null ? authenticator.getClientId() : null;
       result.add(
           newDto(OAuthAuthenticatorDescriptor.class)
               .withName(name)
-              .withEndpointUrl(
-                  authenticator != null
-                      ? authenticator.getEndpointUrl()
-                      : oauth1Providers.getAuthenticator(name).getEndpointUrl())
+              .withEndpointUrl(endpointUrl)
+              .withClientId(clientId)
               .withLinks(links));
     }
     return result;
